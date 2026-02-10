3d1f9b877e6c7c44861e1bdb17e8d11c9d72a61a
Adeoluwa12 <adeoyeoluwaferanmi@gmail.com>
2026-02-10 03:41:33 +0100

    render useless fix puppeteer

--- src/config/puppeteer.ts
+++ src/config/puppeteer.ts
@@ -1,11 +1,6 @@
 export const getPuppeteerConfig = () => {
-  const isProduction = process.env.NODE_ENV === 'production';
-  
   return {
     headless: true,
-    executablePath: isProduction 
-      ? process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium'
-      : undefined,
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
@@ -15,16 +10,6 @@ export const getPuppeteerConfig = () => {
       '--no-zygote',
       '--single-process',
       '--disable-accelerated-2d-canvas',
-      '--disable-background-networking',
-      '--disable-default-apps',
-      '--disable-extensions',
-      '--disable-sync',
-      '--disable-translate',
-      '--hide-scrollbars',
-      '--metrics-recording-only',
-      '--mute-audio',
-      '--no-default-browser-check',
-      '--safebrowsing-disable-auto-update',
     ],
     timeout: 60000,
   };
